6710a6abcffadb69f554a02deb9828079dd7cff1
rana eyp <eyrana002@gmail.com>
2026-03-05 23:17:23 +0300

    feat: QuickCodes added screen grid list and copy functionality

--- lib/quickCodes_screen/quick_codes_screen.dart
+++ lib/quickCodes_screen/quick_codes_screen.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class QuickCodesScreen extends StatefulWidget {
   const QuickCodesScreen({super.key});
@@ -9,6 +9,16 @@ class QuickCodesScreen extends StatefulWidget {
 }
 
 class _QuickCodesScreenState extends State<QuickCodesScreen> {
+  void _copyToClipboard(String text) {
+    Clipboard.setData(ClipboardData(text: text));
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text("$text kopyalandı!")));
+  }
+
+  List<Map<String, String>> quickCodes = [];
+  TextEditingController labelController = TextEditingController();
+  TextEditingController codeController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +29,57 @@ class _QuickCodesScreenState extends State<QuickCodesScreen> {
         elevation: 0,
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          "No quick codes yet.",
-          style: TextStyle(color: Colors.grey),
-        ),
+      body: quickCodes.isEmpty
+          ? const Center(child: Text("Henüz hızlı kod eklenmedi"))
+          : GridView.builder(
+              padding: const EdgeInsets.all(15),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 15,
+                crossAxisSpacing: 15,
+                childAspectRatio: 1.1,
+              ),
+              itemCount: quickCodes.length,
+              itemBuilder: (context, index) {
+                final item = quickCodes[index];
+                return GestureDetector(
+                  onTap: () => _copyToClipboard(item['code']!),
+                  child: Card(
+                    color: Colors.white,
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.pin, color: Colors.green, size: 30),
+                        const SizedBox(height: 10),
+                        Text(
+                          item['label']!,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          item['code']!,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: () => _showAddCodeSheet(context),
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
+
+  void _showAddCodeSheet(BuildContext context) {}
 }
